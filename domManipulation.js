74f43eaba49373d2b59f71041828e85950e31fb0
beeteaa <itspandanbrandon@gmail.com>
2025-02-19 18:06:40 -0600

    added dom manipulation notes

---
+++ domManipulation.js
@@ -0,0 +1,11 @@
+// DOM Manipulation
+
+// DOM (Document Object Model) is the tree structure. 
+// Represents the HTML page
+
+// This is how you use js to read, modify, and interact with web pages dynamically
+
+// keyword: DYNAMICALLY (oh yea)
+
+
+
