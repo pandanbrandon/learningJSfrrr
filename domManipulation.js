aa917aedfa470a9c88acee086a0b0f97d8752a37
beeteaa <itspandanbrandon@gmail.com>
2025-02-19 18:42:37 -0600

    dom manip

--- domManipulation.js
+++ domManipulation.js
@@ -9,3 +9,25 @@
 
 
 
+// selecting elements
+
+let title = document.getElementById("title");
+
+console.log(title); // outputs html, <h1 id="title">Hello fine shyts.</h1>
+
+
+// modify elements
+// with "textContent" property
+
+title.textContent = ("Saluatations fine shyt");
+
+// innerHTML
+title.innerHTML = "<i>Saluatations fine shyt</i>"; // Changes text and renders HTML
+
+
+// ----- Changing Styles -----
+
+title.style.color = "pink"; // Changes text color to blue
+title.style.fontSize = "30px"; // Changes font size to 30px
+
+
